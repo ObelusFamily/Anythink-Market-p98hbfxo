abb99aab021de558de35bebf22b25aec95061774
Prahal <prahal@thebrightapp.xyz>
2022-12-30 13:54:38 +1100

    quest-3

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -54,10 +54,13 @@ const ItemPreview = (props) => {
               className="user-pic rounded-circle pr-1"
             />
           </Link>
-          <div className="flex-grow-1">
-            <img src='/verified_seller.svg' alt='verified' />
-            TOP SELLER
-          </div>
+          {
+            item.seller.isVerified && 
+            <div className="flex-grow-1">
+              <img src='/verified_seller.svg' alt='verified' />
+              TOP SELLER
+            </div>
+          }
           <button className="btn btn-outline-secondary" onClick={handleClick}>
             <i className="ion-heart"></i> {item.favoritesCount}
           </button>
